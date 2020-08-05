417e16038ba619c7c9f4fe41e5bffa16150d4fb1
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-08-05 22:01:25 +0300

    Fetch users & find by userId for each post

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,23 +1,35 @@
 import useSWR from 'swr';
+import { PostType } from '../interfaces/Post';
+import { UserType } from '../interfaces/User';
 import fetcher from '../libs/fetcher';
-import { PostType } from '../modules/Post';
 
 export default function PostList(): JSX.Element {
-  const { data, error } = useSWR(
+  const { data: users, error: usersError } = useSWR(
+    'https://jsonplaceholder.typicode.com/users',
+    fetcher
+  );
+  const { data: posts, error: postsError } = useSWR(
     'https://jsonplaceholder.typicode.com/posts',
     fetcher
   );
 
-  if (error) return <div>Ошибка загрузки данных</div>;
-  if (!data) return <div>Загрузка...</div>;
+  if (postsError || usersError) return <div>Ошибка загрузки данных</div>;
+  if (!posts || !users) return <div>Загрузка...</div>;
+
+  const getUser = (userId: number) => {
+    return users.find((user: UserType) => user.id === userId);
+  };
 
   return (
     <>
-      {data.map(({ id, title, body }: PostType) => (
-        <div key={id}>
+      {posts.map(({ id, userId, title, body }: PostType) => (
+        <article key={id}>
           <h2>{title}</h2>
+          <div>
+            {getUser(userId).name} @{getUser(userId).username}
+          </div>
           <div>{body}</div>
-        </div>
+        </article>
       ))}
     </>
   );
